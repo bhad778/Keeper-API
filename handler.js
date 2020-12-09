536ac1df912656661913b38d01c8491f5d4b3a4c
bhad778@gmail.com <treatmentrecordsolutions@gmail.com>
2020-12-08 19:08:34 -0500

    fixing unexpected lambda output error

--- handler.js
+++ handler.js
@@ -6,60 +6,58 @@ const Employer = require('./models/Employer');
 const Employee = require('./models/Employee');
 const Job = require('./models/Job');
 
-module.exports.addCognitoUserToMongoDb = async (event, context, callback) => {
+module.exports.addCognitoUserToMongoDb = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  if (event.email === 'bhad778@gmail.com') {
+  let newUser = {
+    email: event.request.userAttributes.email,
+    phoneNumber: event.request.userAttributes.phone_number,
+  };
+  event.body = newUser;
+
+  // Send post authentication data to Cloudwatch logs
+  console.log('event.body =', event.body);
+  console.log('event =', event);
+  console.log('event.request = ', event.request);
+  console.log(
+    'event.request.userAttributes.email= ',
+    event.request.userAttributes.email
+  );
+  console.log('context= ', context);
+  console.log('callback= ', callback);
+  context.done(null, event);
+  if (event.request.userAttributes.email === 'bhad778@gmail.com') {
     this.addEmployer(event, context, callback);
   } else if (event.email === 'employe@employee.com') {
     this.addEmployee(event, context, callback);
+  } else {
+    this.addEmployee(event, context, callback);
   }
-  context.done(null, 'ok');
+  context.done(null, event);
 };
 
-module.exports.addEmployer = async (event, context, callback) => {
+module.exports.addEmployer = (event, context, callback) => {
+  console.log('event =', event);
   context.callbackWaitsForEmptyEventLoop = false;
 
   connectToDatabase().then(() => {
     Employer.create(JSON.parse(event.body))
-      .then((employer) =>
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(employer),
-        })
-      )
-      .catch((err) =>
-        callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the employer.',
-        })
-      );
+      .then((employer) => context.done(null, event))
+      .catch((err) => context.done(null, event));
   });
 };
 
-module.exports.addEmployee = async (event, context, callback) => {
+module.exports.addEmployee = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   connectToDatabase().then(() => {
     Employee.create(JSON.parse(event.body))
-      .then((employee) =>
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(employee),
-        })
-      )
-      .catch((err) =>
-        callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the employee.',
-        })
-      );
+      .then((employee) => context.done(null, event))
+      .catch((err) => context.done(null, event));
   });
 };
 
-module.exports.addJob = async (event, context, callback) => {
+module.exports.addJob = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   connectToDatabase().then(() => {
