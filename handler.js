a55d01ae793b793a8e568f44b807d628ac9919bd
bhad778@gmail.com <treatmentrecordsolutions@gmail.com>
2020-12-10 00:04:43 -0500

    fixed cognito trigger function

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const Employer = require('./models/Employer');
 const Employee = require('./models/Employee');
 const Job = require('./models/Job');
 
-module.exports.addCognitoUserToMongoDb = async (event, context, callback) => {
+module.exports.addCognitoUserToMongoDb = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   let newUser = {
@@ -14,26 +14,34 @@ module.exports.addCognitoUserToMongoDb = async (event, context, callback) => {
     phoneNumber: event.request.userAttributes.phone_number,
   };
 
-  // Send post authentication data to Cloudwatch logs
-  console.log('event.body =', event.body);
-  console.log('event =', event);
-  console.log('event.request = ', event.request);
-  console.log(
-    'event.request.userAttributes.email= ',
-    event.request.userAttributes.email
-  );
-  console.log('context= ', context);
-  console.log('callback= ', callback);
-
+  // change to if(userAttribute.type = "employee")
+  // TODO fix error handling
+  // TODO change trigger to post authentication
   if (event.request.userAttributes.email === 'bhad778@gmail.com') {
-    await this.addEmployer(event, context, callback);
-    context.done(null, event);
-  } else if (event.email === 'employe@employee.com') {
-    this.addEmployee(event, context, callback);
-  } else {
-    this.addEmployee(event, context, callback);
+    connectToDatabase().then(() => {
+      Employer.create(newUser)
+        .then(context.done(null, event))
+        .catch((err) =>
+          callback(null, {
+            statusCode: err.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not add the job.',
+          })
+        );
+    });
+  } else if (event.request.userAttributes.email === 'bhad7778@gmail.com') {
+    connectToDatabase().then(() => {
+      Employee.create(newUser)
+        .then(context.done(null, event))
+        .catch((err) =>
+          callback(null, {
+            statusCode: err.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not create the employee.',
+          })
+        );
+    });
   }
-  context.done(null, event);
 };
 
 module.exports.addEmployer = (event, context, callback) => {
