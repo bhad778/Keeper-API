9856dc181dfc65a9b65fd9c8c9c3a3fbddc5c809
bhad778@gmail.com <treatmentrecordsolutions@gmail.com>
2020-12-11 21:11:15 -0500

    added get jobs function

--- handler.js
+++ handler.js
@@ -5,6 +5,7 @@ const connectToDatabase = require('./db');
 const Employer = require('./models/Employer');
 const Employee = require('./models/Employee');
 const Job = require('./models/Job');
+const axios = require('axios');
 
 module.exports.addCognitoUserToMongoDb = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -88,9 +89,25 @@ module.exports.addEmployee = async (event, context, callback) => {
 
 module.exports.addJob = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
+  var body = JSON.parse(event.body);
+  var uriEncodedAddress = encodeURIComponent(body.address);
+
+  await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${uriEncodedAddress}&key=AIzaSyDoUt-NQKYX-8sZU87ISTxNIg7DQijLZ7A`
+    )
+    .then((response) => {
+      body.geoLocation = {
+        type: 'Point',
+        coordinates: [
+          response.data.results[0].geometry.location.lng,
+          response.data.results[0].geometry.location.lat,
+        ],
+      };
+    });
 
   connectToDatabase().then(() => {
-    Job.create(JSON.parse(event.body))
+    Job.create(body)
       .then((job) =>
         callback(null, {
           statusCode: 200,
@@ -104,5 +121,28 @@ module.exports.addJob = async (event, context, callback) => {
           body: 'Could not create the job.',
         })
       );
+  });
+};
+
+module.exports.getJobs = async (event, context, callback) => {
+  var body = JSON.parse(event.body);
+
+  connectToDatabase().then(() => {
+    Job.find({
+      geoLocation: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [body.lng, body.lat],
+          },
+          $maxDistance: body.distance,
+          $minDistance: 1,
+        },
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   });
 };
--- models/Job.js
+++ models/Job.js
@@ -2,5 +2,20 @@ const mongoose = require('mongoose');
 const JobSchema = new mongoose.Schema({
   title: String,
   description: String,
+  address: String,
+  geoLocation: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
 });
+
+JobSchema.index({ geoLocation: '2dsphere' });
+
 module.exports = mongoose.model('Jobs', JobSchema);
